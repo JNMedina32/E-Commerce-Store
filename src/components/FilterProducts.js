ebd1974abf4ad2d5e231014480e719fa96effa49
JNMedina32 <JNMedina32.github.io>
2023-06-17 04:48:56 -0400

    Late night commit

--- src/components/FilterProducts.js
+++ src/components/FilterProducts.js
@@ -3,8 +3,14 @@
 
 export default function FilterProducts(props) {
   return (
-    <div className="col filterBar">
-      FilteredProducts
+    <div className="list-group list-group-horizontal">
+      <button className="list-group-item list-group-item-action  active">All</button>
+      <button className="list-group-item list-group-item-action ">Mens</button>
+      <button className="list-group-item list-group-item-action ">Women</button>
+      <button className="list-group-item list-group-item-action ">Boys</button>
+      <button className="list-group-item list-group-item-action ">Girls</button>
     </div>
-  )
+  );
 }
+
+
