6e12a77ed489315c364acbc6cad56a78149d2493
Joshua Medina <JNMedina32@gmail.com>
2023-09-30 12:10:15 -0400

    Routes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/E-Commerce-Store/',
+  base: 'https:://jnmedina32.github.io/E-Commerce-Store/',
   plugins: [react()],
 })
